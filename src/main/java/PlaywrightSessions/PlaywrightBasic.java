d55b943ac2add736f0bb1aad65d9e381b1f8717f
momtazrasel <momtazrasel8@gmail.com>
2024-11-07 22:47:13 +0600

    Playwright First automation

---
+++ src/main/java/PlaywrightSessions/PlaywrightBasic.java
@@ -0,0 +1,17 @@
+package PlaywrightSessions;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightBasic {
+    public static void main(String[] args) {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        page.navigate("https://www.amazon.com/");
+        System.out.println(page.title());
+    }
+
+}

eeb329a53cfcc1fc877bc20de64997f7d0e1875f
momtazrasel <momtazrasel8@gmail.com>
2024-11-15 22:19:48 +0600

    Dropdown test

--- src/main/java/PlaywrightSessions/DropdownText.java
+++ src/main/java/PlaywrightSessions/DropdownText.java
@@ -4,6 +4,10 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.MouseButton;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class DropdownText {
@@ -26,6 +30,53 @@ public class DropdownText {
             assertThat(page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("logo").setExact(true))).isVisible();
             page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Site").setExact(true)).click();
             page.getByRole(AriaRole.COMBOBOX).click();
+            page.waitForTimeout(5000);
+
+            page.waitForSelector("//div[@class = 'ant-select-item-option-content']");
+            Set<String> dropdownValues = new HashSet<>();
+            while (true) {
+                String highlightedOption = page.locator("(//div[contains(text(),'00 Calibration Gateway')])[1]").textContent();
+                if (highlightedOption != null && dropdownValues.add(highlightedOption)) {
+                    System.out.println(highlightedOption);
+                }
+                for(int i=0; i<=100; i++){
+                    page.keyboard().press("ArrowDown");
+                    System.out.println(i);
+                }
+
+                page.waitForTimeout(200);
+                String lastOption = page.locator("(//div[@class='ant-select-item-option-content'])[last()]").textContent();
+                if (dropdownValues.contains(lastOption)) {
+                    break; // Exit the loop once the last option is reached
+                }
+
+                // Get the current number of elements
+                int currentOptionsCount = page.locator("//div[@class = 'ant-select-item-option-content']").count();
+                System.out.println("Number of elements: "+ currentOptionsCount);
+
+                // Scroll to the last visible option
+                page.locator("div.dropdown-option:last-child").scrollIntoViewIfNeeded();
+
+                // Wait for a short period to allow new options to load
+                page.waitForTimeout(500);
+
+                // Check if more options have been loaded
+                int updatedOptionsCount = page.locator("div.dropdown-option").count();
+                if (updatedOptionsCount == currentOptionsCount) {
+                    break; // Exit the loop if no new options are loaded
+                }
+            }
+
+            // Get all dropdown values
+//            List<String> dropdownValues = page.locator("div.dropdown-option").allTextContents();
+
+            // Print all values
+            for (String value : dropdownValues) {
+                System.out.println(value);
+            }
+
+            // Close the browser
+            browser.close();
         }
     }
 }

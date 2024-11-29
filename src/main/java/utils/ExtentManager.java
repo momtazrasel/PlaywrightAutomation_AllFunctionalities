9b3383250262f0ecdbda8b812a00f1533ba12dc6
momtazrasel <momtazrasel8@gmail.com>
2024-11-29 18:00:44 +0600

    extent report

---
+++ src/main/java/utils/ExtentManager.java
@@ -0,0 +1,28 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    public static void initReports() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("reports/AutomationReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    public static ExtentTest createTest(String testName) {
+        test = extent.createTest(testName);
+        return test;
+    }
+
+    public static void flushReports() {
+        if (extent != null) {
+            extent.flush();
+        }
+    }
+}
